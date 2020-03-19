855ae8eb14f2b6966e94abfb73a61dbf788c0e78
nth15 <nth15@hi.is>
2020-03-19 13:53:23 +0000

    readme

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Fitness tracker backend
 ### Hugbúnaðarverkefni 2 2020
 https://hugbo2-2020.herokuapp.com/
 
+[Github App](https://github.com/arnarsnaeland/Fitness-tracker-framendi)
+
+
 Authors
 =========
 - Nökkvi Þórsson (nth15@hi.is)
--- src/main/java/is/hi/hbv503/FitnessTracker/FitnessTracker/Controllers/ExerciseController.java
+++ src/main/java/is/hi/hbv503/FitnessTracker/FitnessTracker/Controllers/ExerciseController.java
@@ -1,8 +1,12 @@
 package is.hi.hbv503.FitnessTracker.FitnessTracker.Controllers;
 
-import is.hi.hbv503.FitnessTracker.FitnessTracker.Entities.*;
 import is.hi.hbv503.FitnessTracker.FitnessTracker.Services.ExerciseService;
 import is.hi.hbv503.FitnessTracker.FitnessTracker.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import is.hi.hbv503.FitnessTracker.FitnessTracker.Entities.*;
 import is.hi.hbv503.FitnessTracker.FitnessTracker.Wrappers.Responses.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
